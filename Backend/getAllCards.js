e3102fe5a9de24feb7514de02c63e6be1a256d97
Kemoe19 <kemoe19@student.sdu.dk>
2023-04-18 10:12:39 +0200

    getImagesFromIndernet

---
+++ Backend/getAllCards.js
@@ -0,0 +1,33 @@
+let mysql = require("mysql2");
+let cards;
+
+let con = mysql.createPool({
+  connectionLimit: 10, // default = 10
+  host: "localhost",
+  user: "root",
+  password: "secret",
+  database: "mtg",
+});
+
+function getTableData(tableName, callback) {
+    const sql = `SELECT * FROM ${tableName}`;
+  
+    con.query(sql, (error, results) => {
+      if (error) {
+        console.error(error);
+        callback(error, null);
+      } else {
+        callback(null, results);
+      }
+    });
+    
+  }
+  
+
+  
+
+  
+  // Export the function so it can be used in other modules
+  module.exports = {getTableData};
+  
+ 

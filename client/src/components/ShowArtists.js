7d48ab6eef000252b0d616f7b816999c3dfb65da
BJLUN21 <bjlun21@student.sdu.dk>
2023-05-14 13:05:59 +0200

    Tilføjet lidt statistik til ShowArtist.js

--- client/src/components/ShowArtists.js
+++ client/src/components/ShowArtists.js
@@ -15,6 +15,10 @@ function ShowArtists() {
       });
   }, [setCode]);
 
+  if (backendData === null || backendData === undefined || backendData.length <= 1) {
+    return <div>Loading . . .</div>
+  }
+
   function getArtistNames(cards) {
     let artistNames = [];
     let artist;
@@ -31,6 +35,37 @@ function ShowArtists() {
     setTheArtist(name);
   }
 
+  //Gets total count of cards in set
+  function getTotalCount(cards) {
+    let totalCount = 0;
+    if (!cards) {
+      return totalCount;
+    }
+    for (let i = 0; i < cards.length; i++) {
+      totalCount ++;
+    }
+    return totalCount;
+  }
+
+  //Finds the number of images created by artist
+  function getArtCount(cards, artist) {
+    let artCount = 0;
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].artist === artist) {
+        artCount ++;
+      }
+    }
+    return artCount;
+  }
+
+  //Calculates the percentage of cards created by artist within the set
+  function getPercentage(totalCount, artCount) {
+    let num = (artCount/totalCount) * 100;
+    return parseFloat((num + 0.005).toFixed(2)) ;
+  }
+
+  console.log(backendData);
+
   return (
     <div className="page-view">
       <div className="artistsTable">
@@ -40,6 +75,7 @@ function ShowArtists() {
         {getArtistNames(backendData).map(artist => (
           <div className="artist" key={artist} onClick={() => showArtByArtist(artist)} >
             <h3>{artist}</h3>
+            <h5>{getPercentage(getTotalCount(backendData), getArtCount(backendData, artist))}% ({getArtCount(backendData, artist)} of {getTotalCount(backendData)})</h5>
           </div>
         ))}
       </div>
